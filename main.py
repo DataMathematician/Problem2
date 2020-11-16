8ef2cdc27e02233968fa92f9f4dcdb074ab47044
DataMathematician <datamathematician@gmail.com>
2020-11-16 14:01:51 +0300

    main.py update
    Переделана логика, ищет в одном файле за раз.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import glob
-from multiprocessing import Process
+#from multiprocessing import Process
+import pandas as pd
 
 class FileSearcher():
     def __call__(self,name,lfn,path_to_write):
@@ -11,53 +12,30 @@ class FileSearcher():
             Вернет кол-во вхождений
             '''
             with open(name,'r') as current_csv:
-                #reader = csv.reader(current_csv)
                 global_count = 0
                 for row in current_csv:
-                    #row = map(int(),row)
                     global_count += row.count(str(lfn))
             return global_count
 
         def csv_writer(name,lfn,global_count):
+            fieldnames = ['file_name','looking_for_number','count']
             data = {'file_name': name,'looking_for_number':lfn,'count':global_count}
-            with open(path_to_write,'a') as local_csv:
-                writer = csv.writer(local_csv,delimiter=',')
-                #for row in data:
-                writer.writerow(data)
-
-        #def main(name,lfn,path_to_write):
+            df = pd.DataFrame(data = data,index =[0])
+            with open(path_to_write, 'a') as f:
+                df.to_csv(f, header=f.tell()==0,index =False)
+            
+                
         global_count = csv_reader(name,lfn)
         csv_writer(name,lfn,global_count)
 
 if __name__ == '__main__':
     
     lfn = 10
-    n_processes = 10
-    c_processes = 0
     files = glob.glob("D:\\sber\\Problem2\\*.csv")
     path_to_write = "D:\\sber\\Problem2\\stats.csv" # + filename
-    procs = []
-    while True:
-        try:
-            if len(files) != 0 and c_processes < n_processes:  
-                name = files.pop()
-                c_processes += 1
-                t = FileSearcher()
-                proc = Process(target = t,kwargs={'name':name,'lfn':lfn,'path_to_write':path_to_write})
-                procs.append(proc)
-                proc.start()
-                #if len(files) != 0:
-                #    continue
-            elif len(files) == 0:
-                c_processes -= 1
-                proc.join()
-            elif c_processes == n_processes:
-                proc.join()
-        except:
-            if len(files) == 0: # если вдруг перед потоком, другой поток украл последний файл
-                for proc in procs:
-                    proc.join()
-            elif c_processes == n_processes:
-                continue
-            else:
-                continue
+    
+    while len(files) != 0:
+        name = files.pop()
+        t = FileSearcher()
+        t.__call__(name,lfn,path_to_write)
+           
