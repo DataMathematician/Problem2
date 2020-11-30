040523651f106d97e35b2494b214b470d043566c
DataMathematician <datamathematician@gmail.com>
2020-11-30 10:30:41 +0300

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from app import db, session, Base
-
-class BigData(Base):
-    __tablename__ = 'csv_reports'                   # имя табл в базе
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(250),nullable=False)
-    volume = db.Column(db.Integer,nullable = False)
